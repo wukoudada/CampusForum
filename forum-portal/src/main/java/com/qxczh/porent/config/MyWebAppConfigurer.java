2ea7c754438ada1311d486f54098c9038feb434c
wukoudada <710138642@qq.com>
2021-12-03 01:27:47 +0800

    xxx

---
+++ forum-portal/src/main/java/com/qxczh/porent/config/MyWebAppConfigurer.java
@@ -0,0 +1,35 @@
+//package com.qxczh.porent.config;
+//
+///**
+// * TODO
+// *
+// * @author wkdd
+// * @version 1.0
+// * @date 2021/12/3 0:42
+// */
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//
+///**
+// * 资源映射路径
+// */
+////@CrossOrigin
+//@Configuration
+//public class MyWebAppConfigurer extends WebMvcConfigurationSupport {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        /**
+//         * 资源映射路径
+//         * addResourceHandler：访问映射路径
+//         * addResourceLocations：资源绝对路径
+//         */
+////        registry.addResourceHandler("/images/**").addResourceLocations("file:D://home//images//");
+////        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+////        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+//
+//        super.addResourceHandlers(registry);
+//    }
+//}
---
+++ forum-rest/src/main/java/com/qxczh/rest/config/MyWebAppConfigurer.java
@@ -0,0 +1,32 @@
+//package com.qxczh.rest.config;
+//
+///**
+// * TODO
+// *
+// * @author wkdd
+// * @version 1.0
+// * @date 2021/12/3 0:42
+// */
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//
+///**
+// * 资源映射路径
+// */
+//@CrossOrigin
+//@Configuration
+//public class MyWebAppConfigurer extends WebMvcConfigurationSupport {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        /**
+//         * 资源映射路径
+//         * addResourceHandler：访问映射路径
+//         * addResourceLocations：资源绝对路径
+//         */
+//        registry.addResourceHandler("/upload/**").addResourceLocations("file:D://home//images//upload//");
+//        super.addResourceHandlers(registry);
+//    }
+//}
